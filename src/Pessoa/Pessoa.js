ef2e8a9feb4fd0277bc9d9339033c9100f41819d
mylenaverspeelt <mylena49@hotmail.com>
2022-12-04 10:17:12 -0300

    feat: testes classe Pessoa

--- src/Pessoa/Pessoa.js
+++ src/Pessoa/Pessoa.js
@@ -1,16 +1,27 @@
 class Pessoa {
     id
     nomeCompleto
+    dataDeNascimento
     idade
     telefone
-    dataDeNascimento
+
+
     constructor(id, nomeCompleto, dataDeNascimento, idade, telefone) {
-        this.id = id
-        this.nomeCompleto = nomeCompleto
-        this.dataDeNascimento = dataDeNascimento
-        this.idade = idade
-        this.telefone = telefone
+        if (typeof id === 'number' || typeof nomeCompleto === 'string' || typeof dataDeNascimento === 'string' || typeof idade === 'number' || typeof telefone === 'string') {
+            this.id = id
+            this.nomeCompleto = nomeCompleto
+            this.dataDeNascimento = dataDeNascimento
+            this.idade = idade
+            this.telefone = telefone
+        }
+
+
     }
 }
 
+
 module.exports = Pessoa
+
+let pessoa1 = new Pessoa('banana')
+
+console.log(pessoa1);
---
+++ src/Pessoa/pessoa.test.js
@@ -0,0 +1,32 @@
+const Pessoa = require('./Pessoa')
+
+describe('Testes da classe Pessoa', () => {
+    test('Fazer uma instância da classe Pessoa com sucesso', () => {
+        let novaPessoa = new Pessoa(10, 'Maria das Dores', '31/08/2000', 22, '4002-8922')
+        expect(novaPessoa).toEqual({ "dataDeNascimento": "31/08/2000", "id": 10, "idade": 22, "nomeCompleto": "Maria das Dores", "telefone": "4002-8922" })
+    })
+    // test('Fazer uma instância da classe Pessoa sem sucesso', () => {
+    //     let novaPessoa = new Pessoa()
+    //     expect(() => (novaPessoa)).toThrow('Não foi possível criar uma instância de Pessoa')
+    // })
+    test('Verificar nome da pessoa', () => {
+        let novaPessoa = new Pessoa(10, 'Maria das Dores', '31/08/2000', 22, '4002-8922')
+        expect(novaPessoa.nomeCompleto).toBe('Maria das Dores')
+    })
+    test('Verificar idade da pessoa', () => {
+        let novaPessoa = new Pessoa(10, 'Maria das Dores', '31/08/2000', 22, '4002-8922')
+        expect(novaPessoa.idade).toBe(22)
+    })
+    test('Verificar data de nascimento', () => {
+        let novaPessoa = new Pessoa(10, 'Maria das Dores', '31/08/2000', 22, '4002-8922')
+        expect(novaPessoa.dataDeNascimento).toBe('31/08/2000')
+    })
+    test('Verificar id', () => {
+        let novaPessoa = new Pessoa(10, 'Maria das Dores', '31/08/2000', 22, '4002-8922')
+        expect(novaPessoa.id).toBe(10)
+    })
+    test('Verificar telefone para contato', () => {
+        let novaPessoa = new Pessoa(10, 'Maria das Dores', '31/08/2000', 22, '4002-8922')
+        expect(novaPessoa.telefone).toBe('4002-8922')
+    })
+})
