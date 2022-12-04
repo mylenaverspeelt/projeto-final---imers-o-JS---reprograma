5a4fafe6ce41eb56dcf51fa0ba758afa5ee1f4e0
mylenaverspeelt <mylena49@hotmail.com>
2022-12-04 08:27:00 -0300

    feat: classe Farmacia

---
+++ src/Farmacia/Farmacia.js
@@ -0,0 +1,38 @@
+const Entregador = require('../Entregador/Entregador')
+const Paciente = require('../Paciente/Paciente')
+const Pessoa = require('../Pessoa/Pessoa')
+
+
+class Farmacia {
+    nomeFantasia
+    cnpj
+    medicamentosEmEstoque
+
+
+    constructor(nomeFantasia, cnpj) {
+        this.nomeFantasia = nomeFantasia
+        this.cnpj = cnpj
+        this.medicamentosEmEstoque = estoque
+    }
+
+    verificarEstoque() {
+        //codigo
+    }
+
+    gerarNumeroDePedido() {
+        //codigo
+
+    }
+
+    gerarNotaFiscal() {
+        //codigo
+    }
+
+
+    fazerPedido() {
+        //codigo
+    }
+}
+
+module.exports = Farmacia
+
