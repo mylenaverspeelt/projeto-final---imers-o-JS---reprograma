09f1f643145a32ef4fe9c98d74f2dd5e3e9efcfa
mylenaverspeelt <mylena49@hotmail.com>
2022-12-08 21:20:18 -0300

    gitignore

--- src/Entregador/entregador.test.js
+++ src/Entregador/entregador.test.js
@@ -21,6 +21,7 @@ describe('Testes da classe Entregador', () => {
     test('Verificar se o condutor é maior de idade ', () => {
         expect(() => new Entregador(new Pessoa(1, 'Maria das Dores', '31/08/2000', 13, '4002-8922'), 'carro', 989, true)).toThrow('Motorista menor de idade. Não foi possível criar uma instância')
     })
+// CONSERTAR TESTE
     test('Fazer uma instância com valores indefinidos', () => {
         expect(() => new Entregador(new Pessoa(1, 'Maria das Dores', '31/08/2000', 22, '4002-8922'), 'carro', 989)).toThrow('Não é possível instanciar com valores indefinidos')
     });
